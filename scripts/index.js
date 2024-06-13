2247d27c6d0477bd24ddde0fd0cd60e37b88b768
vmpgonzalez <vpacheco17@gmail.com>
2024-06-13 05:29:25 -0700

    project4final

--- scripts/index.js
+++ scripts/index.js
@@ -45,7 +45,7 @@ const cardTemplate =
 /*Functions*/
 
 function closePopup() {
-  profileEditModal.classList.remove("modal__opened");
+  profileEditModal.classList.remove("modal_opened");
 }
 
 function getCardElement(cardData) {
@@ -72,7 +72,7 @@ function handleProfileEditSubmit(e) {
 profileEditButton.addEventListener("click", () => {
   profileTitleInput.value = profileTitle.textContent;
   profileDescriptionInput.value = profileDescription.textContent;
-  profileEditModal.classList.add("modal__opened");
+  profileEditModal.classList.add("modal_opened");
 });
 
 profileModalCloseButton.addEventListener("click", closePopup);
